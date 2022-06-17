baf1f25de6cf7c036251b859bd281a1df0058e3c
Rodrigo Gualvari <rodrigogualvari@gmail.com>
2022-06-17 19:22:14 -0300

    acordeon.jsx

--- src/components/commons/Acordeon.jsx
+++ src/components/commons/Acordeon.jsx
@@ -4,8 +4,18 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { menuOptions } from "../utils/menuOptionsList";
 
 export default function SimpleAccordion() {
+  const optionsToShow = [
+    "estadoCuenta",
+    "modificarDatos",
+    "promociones",
+    "pagoRecarga",
+    "soporte",  
+  ];
+
+
   return (
     <div>
       <Accordion>
@@ -14,7 +24,11 @@ export default function SimpleAccordion() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography>
+          {optionsToShow.map((option) => {
+            return menuOptions[option];
+          })}
+          </Typography>
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -23,30 +37,7 @@ export default function SimpleAccordion() {
           </Typography>
         </AccordionDetails>
       </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion disabled>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography>Disabled Accordion</Typography>
-        </AccordionSummary>
-      </Accordion>
+      
     </div>
   );
 }
