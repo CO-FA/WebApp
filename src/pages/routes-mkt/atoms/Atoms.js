2a2d88b19269bbe60ea8be9105f70653886ef6e6
Leandro Canteruccio <l.canteruccio@hotmail.com>
2022-10-29 15:20:44 -0300

    add atoms storage

--- src/pages/routes-mkt/atoms/Atoms.js
+++ src/pages/routes-mkt/atoms/Atoms.js
@@ -5,3 +5,23 @@ export const useStepAtom = () => {
   const [currentStep, setCurrentStep] = useAtom(stepAtom);
   return { currentStep, setCurrentStep };
 };
+
+const documentoAtom = atom(-1);
+const nombreAtom = atom("");
+const cuitAtom = atom("");
+export const useIdentidadAtom = () => {
+  const [documento, setDocumento] = useAtom(documentoAtom);
+  const [nombre, setNombre] = useAtom(nombreAtom);
+  const [cuit, setCuit] = useAtom(cuitAtom);
+  return { documento, setDocumento, nombre, setNombre, cuit, setCuit };
+};
+
+const codAreaAtom = atom("");
+const numCelularAtom = atom("");
+const pinAtom = atom("");
+export const useCelularAtom = () => {
+  const [codArea, setCodArea] = useAtom(codAreaAtom);
+  const [numCelular, setNumCelular] = useAtom(numCelularAtom);
+  const [pin, setPin] = useAtom(pinAtom);
+  return { codArea, setCodArea, numCelular, setNumCelular, pin, setPin };
+};
