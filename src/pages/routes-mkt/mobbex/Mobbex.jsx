82162281d1337389efd69ec0fd7551da08cf368c
antomag <antonellamagnano@gmail.com>
2023-04-11 15:48:04 -0300

    create mobbex page

---
+++ src/pages/routes-mkt/mobbex/Mobbex.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../../assets/css/main.css";
+
+export function Mobbex() {
+    return(
+        <div className="mobbex-cont">
+            <h1 className="mobbex">VOY A MOBBEX y vuelvo</h1>
+            {/* falta funcion que vuelva. settimeout? */}
+        </div>
+    );
+}
