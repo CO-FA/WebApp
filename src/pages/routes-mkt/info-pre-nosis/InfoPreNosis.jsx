e2d93db9f714c1b44e710ac17daeab7e42e904d5
antomag <antonellamagnano@gmail.com>
2023-04-13 12:02:04 -0300

    create page info pre nosis

--- src/pages/routes-mkt/info-pre-nosis/InfoPreNosis.jsx
+++ src/pages/routes-mkt/info-pre-nosis/InfoPreNosis.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import Encabezado from "components/commons/Encabezado";
+import { Formik } from "formik";
+import { useHistory } from "react-router-dom";
+import { formErrors } from "../../../utils/constantsErrors";
+import Input from "components/commons/Input";
+import Footer from "components/commons/Footer";
+import Button from "components/commons/Button";
+
+import RegistroSetps from "../../../components/registro/RegistroSteps";
+import { STEPS } from "../../../components/registro/constantsSteps";
+import { LoaderContext } from "../../../components/loader/LoaderContext";
 
 export function InfoPreNosis() {
     return(
-        <p>Componente verificar info antes de nosis</p>
-    );
+        <>
+        <Encabezado/>
+        <Formik
+            /* onSubmit={(values, { setSubmitting }) =>
+            submitForm(values, setSubmitting)
+            }
+            validate={(values) => validateForm(values)} */
+        >
+            {({
+            values,
+            handleSubmit,
+            /* and other goodies */
+            }) => (
+            <>
+                <form>
+                    <section>
+                        <h3>¡Último paso!</h3>
+                        <div className="row profile-container">
+                            <div className="form-group col-12">
+                                <h4>Ya tenés APROBADO el préstamo por </h4>
+                                <h4>[[var importe]]</h4>
+                                <p>En {}x cuotas de ${}</p>
+                            </div>
+                            <div className="form-group col-12">
+                                <h3>Vamos a validar tu identidad</h3>
+                                <p>Necesitamos:<br/>Foto del DNI, frente y dorso <br/>Una selfie </p>
+                            
+                            </div>
+                        </div>
+                    </section>
+                    <Footer>
+                        <div className="col-12">
+                        <Button
+                            className="btn btn-primary cont"
+                            disabled={false}
+                            type="submit"
+                            onClick={handleSubmit}
+                        >
+                            Continuar
+                        </Button>
+                        </div>
+                    </Footer>
+                </form>
+            </>
+            )}
+        </Formik>
+    </>
+);
 }
