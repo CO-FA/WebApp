dbf721b3c6420a8d0aa627df646ff7bdc8d8666b
antomag <antonellamagnano@gmail.com>
2023-04-17 18:39:49 -0300

    sin clase

--- src/pages/routes-mkt/mobbex/Mobbex.jsx
+++ src/pages/routes-mkt/mobbex/Mobbex.jsx
@@ -3,9 +3,11 @@ import "../../../assets/css/main.css";
 
 export function Mobbex({urlMobbex}) {
     return(
-        <div className="mobbex-cont">
-            {/* falta funcion que vuelva. settimeout? */}
+        <>
             <iframe style={{paddingRight:"-15px", paddingLeft:"-15px"}} src={urlMobbex}></iframe>
-        </div>
+        </>
+        
     );
 }
+
+
