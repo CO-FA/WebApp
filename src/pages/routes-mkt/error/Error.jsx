826685138dd5532018bc128b53366493cc86237f
Antonella M <antonellamagnano@gmail.com>
2023-08-25 11:00:26 -0300

    style page

--- src/pages/routes-mkt/error/Error.jsx
+++ src/pages/routes-mkt/error/Error.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { useError } from "./hooks/useError";
 import Button from "components/commons/Button";
 import Footer from "components/commons/Footer";
+import "../../../assets/css/main.css"
 
 export function Error() {
   const { volver } = useError();
 
   return (
     <>
-      <div className="text-center" style={{
+      <div className="d-flex flex-column justify-content-center align-items-center"
+      style={{
         background: "#FFD233",
         width: "100vw !important",
         height: "100vh",
@@ -17,9 +19,10 @@ export function Error() {
         marginRight: "-15px",
       }}
       >
-        <h3>En estos momentos no tenemos un préstamo para ofrecerte. </h3>
+        <h3 className="text-center" style={{marginTop:"30vh" }}>En estos momentos no tenemos un préstamo para ofrecerte. </h3>
+        <div style={{ flex: 1 }}></div>
         <Footer>
-          <Button onClick={volver} className="btn btn-light cont">
+          <Button onClick={volver} className="btn btn-light cont shadow">
             VOLVER
           </Button>
         </Footer>
