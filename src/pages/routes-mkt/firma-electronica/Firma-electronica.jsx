ff4b5fade05c73e022a6f7d6e7a6ff4697a60848
Antonella M <antonellamagnano@gmail.com>
2023-09-13 10:37:23 -0300

    new function

--- src/pages/routes-mkt/firma-electronica/Firma-electronica.jsx
+++ src/pages/routes-mkt/firma-electronica/Firma-electronica.jsx
@@ -1,40 +1,7 @@
 import React from "react";
 import "../../../assets/css/main.css";
-import { LoaderContext } from "components/loader/LoaderContext";
-import { STEPS } from "components/registro/STEPS-MKT";
-import {  useLeadAtom, useStepAtom } from "pages/routes-mkt/atoms/Atoms";
-import { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import { statusFirmaSupabase } from "api/StatusFirmaElectronica";
-import { Formik } from "formik";
 
 export default function FirmaElectronica() {
-    /* let { setShowLoader } = React.useContext(LoaderContext);
-    const { setCurrentStep } = useStepAtom();
-    const [errors, setErrors] = useState(false);
-    const history = useHistory();
-    const { lead } = useLeadAtom();
-
-    useEffect(() => {
-      const fetchData = async () => {
-        setShowLoader(true);
-        try {
-          await statusFirmaSupabase({ lead });
-          history.push("/onboarding/prestamo-exitoso");
-          setCurrentStep(STEPS.STEP_13_PRESTAMO_EXITOSO);
-        } catch (error) {
-          history.push("/onboarding/error");
-          setCurrentStep(STEPS.STEP_99_ERROR);
-          console.error(error);
-        } finally {
-          setShowLoader(false);
-        }
-      };
-    
-      fetchData();
-    }, []);
-       */
-
     return(
         <div className="bg-gradient d-flex justify-content-center align-items-center" 
             style={{
---
+++ src/pages/routes-mkt/firma-electronica/hooks/useFirmaElectronica.js
@@ -0,0 +1,32 @@
+import { STEPS } from "components/registro/STEPS-MKT";
+import {  useLeadAtom, useStepAtom } from "pages/routes-mkt/atoms/Atoms";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { statusFirmaSupabase } from "api/StatusFirmaElectronica";
+import { useLoaderContext } from "components/loader/LoaderContext";
+
+export const useFirmaElectronica = () => {
+    const { setShowLoader } = useLoaderContext();
+    const { setCurrentStep } = useStepAtom();
+    const history = useHistory();
+    const { lead } = useLeadAtom();
+
+    useEffect(() => {
+      const fetchData = async () => {
+        setShowLoader(true);
+        try {
+          await statusFirmaSupabase({ lead });
+          history.push("/onboarding/prestamo-exitoso");
+          setCurrentStep(STEPS.STEP_13_PRESTAMO_EXITOSO);
+        } catch (error) {
+          history.push("/onboarding/error");
+          setCurrentStep(STEPS.STEP_99_ERROR);
+          console.error(error);
+        } finally {
+          setShowLoader(false);
+        }
+      };
+    
+      fetchData();
+    }, []);
+};
