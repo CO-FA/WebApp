d394e4a528d8f92e0822cd06139ac5459f677042
Antonella M <antonellamagnano@gmail.com>
2023-09-13 10:51:55 -0300

    fix code

--- backend/services/statusFirmaElectronica.js
+++ backend/services/statusFirmaElectronica.js
@@ -7,37 +7,45 @@ import { updateStatusFirma } from "./validateLoan";
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabase = createClient(supabaseUrl, process.env.SUPABASE_KEY);
 
-//TO DO: chequear con Lean
 const chequearStatusFirma = async (cuit) => {
-    let { data: leadRecupado, error } = await supabase
-    .from('leads')
-    .select('status_firma_electronica')
-    .where('status_firma_electronica' == "Pendiente de firma")
-    .eq("cuit", cuit)
-    console.log("lead", leadRecupado)
-    return leadRecupado
-}; 
+  try {
+    // Corregir la condición y agregar await
+    let { data: leadRecuperado, error } = await supabase
+      .from('leads')
+      .select('status_firma_electronica') 
+      //.where('status_firma_electronica' == "Pendiente de firma") ??
+      .eq("cuit", cuit)
+      .single(); 
 
-export const statusFirmaElectronica = async ({lead}) => {
-    const {cuit, sb_id_prestamo, sb_id_cliente, monto, cuotas, importeCuota } = lead
-    
-    //cada 5  10 min ir a supa a buscar lead con status pendiente de firma. 
-    var cron = require('node-cron');
+    console.log("lead", leadRecuperado);
+    return leadRecuperado;
+  } catch (error) {
+    console.error("Error al recuperar el lead:", error);
+    throw error; 
+  }
+};
 
-    cron.schedule('5 * * * *', async () => {
-        await chequearStatusFirma(cuit)
-    });
+export const statusFirmaElectronica = async ({ lead }) => {
+  const { cuit, sb_id_prestamo, sb_id_cliente, monto, cuotas, importeCuota } = lead;
+
+  var cron = require('node-cron');
+  cron.schedule('*/5 * * * *', async () => { 
+    try {
+      await chequearStatusFirma(cuit);
+    } catch (error) {
+      console.error("Error al verificar el estado de firma:", error);
+    }
+  });
 
   try {
-    //tomo el id prestamo y voy a sb a ver si firmo o no.
     const token = await getToken();
     const myHeaders = new Headers();
     myHeaders.append("Authorization", "Bearer " + token?.token);
     myHeaders.append("Content-type", "application/json");
-    
+
     const body = {
-	    idPrestamo: sb_id_prestamo,
-        accion: 0
+      idPrestamo: sb_id_prestamo,
+      accion: 0
     };
 
     const requestOptions = {
@@ -50,47 +58,45 @@ export const statusFirmaElectronica = async ({lead}) => {
     const resp = await fetch(URL + "/API/v1/loans/firmaElectronica", requestOptions);
     const dataStatusFirmaSB = await resp.json();
 
-    console.log("estado firma SB", dataStatusFirmaSB)
+    console.log("estado firma SB", dataStatusFirmaSB);
 
-    //TO DO: chequear cual es mensaje correcto
     if (dataStatusFirmaSB.estadoFirma === "Firmado") {
-        //dispara el envio del prestamo desde el back.
-        const body1 = {
-            idCliente: sb_id_cliente,
-            fechaAlta: new Date(),
-            comercializadora_Sucursal: 1,
-            monto: monto,
-            cuotas: cuotas,
-            lineaCredito: 1,
-            destinoFondos: 1,
-            importeCuota: importeCuota,
-            primerVto: null,
-            formaPago: 2,
-            estado: 0,
-            referencia: "Préstamo de prueba"
-        };
-    
-        const requestOptions = {
-          method: "POST",
-          headers: myHeaders,
-          body: JSON.stringify(body1),
-          redirect: "follow",
-        };
-    
-        const resp = await fetch(URL + "/API/v1/loans/new", requestOptions);
-        const dataAltaPrestamo = await resp.json();
-        
-        console.log("altaPrestamo", dataAltaPrestamo)
+      const body1 = {
+        idCliente: sb_id_cliente,
+        fechaAlta: new Date(),
+        comercializadora_Sucursal: 1,
+        monto: monto,
+        cuotas: cuotas,
+        lineaCredito: 1,
+        destinoFondos: 1,
+        importeCuota: importeCuota,
+        primerVto: null,
+        formaPago: 2,
+        estado: 0,
+        referencia: "Préstamo de prueba"
+      };
 
-        //actualizo status supabase
-        const statusFirma = "Firmado"
-        await updateStatusFirma(statusFirma, lead)
+      const requestOptions1 = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(body1),
+        redirect: "follow",
+      };
+
+      const resp1 = await fetch(URL + "/API/v1/loans/new", requestOptions1);
+      const dataAltaPrestamo = await resp1.json();
+
+      console.log("altaPrestamo", dataAltaPrestamo);
+
+      const statusFirma = "Firmado";
+      await updateStatusFirma(statusFirma, lead);
     }
 
-    console.log("prestamo enviado! :)")
+    console.log("¡Prestamo enviado! :)");
 
   } catch (error) {
-    console.log(error);
+    console.error("Error general:", error);
+    throw error;
   }
   return Promise.resolve(null);
 };
